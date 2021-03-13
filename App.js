a43241dbe3f5e1a941e465dc21b7424a048f2c41
Srija <srijaghosh2015@gmail.com>
2021-03-12 19:41:15 -0500

    commit on 'master'

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View, TouchableOpacity, Keyboard} from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+
+  const [taskItems, setTaskItems] = useState([]);
+
+  {/* when calling a function, use () => functionName(). Sometimes this can be used through onClick */}
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    {/* Puts everything in taskItems in a new array, and then appends task*/}
+    setTaskItems([...taskItems, task])
+    setTask(null)
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy); 
+  }
+
+  const sortByDate = (a,b) => {
+    return a.date - b.date;
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Todat's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Expiration Date Tracker</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go: /* This will get the taskItems array (the one where new tasks get added to, and will display them through the map iteration) */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item}/>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+
+      </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView 
+        behavior ={Platform.OS == "ios" ? "padding" : "height"} 
+        style={styles.writeTaskWrapper}>
+          <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+          {/* Add Button */}
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+
+
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -14,8 +68,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    paddingTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 50,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
+    borderWidth: 1,
+  },
+  datePicker: {
+    paddingVertical: 15,
+    width: 75,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
