cd3bea2a2f32b0dc2e43785e3a33ccdf7a07054a
Srija <srijaghosh2015@gmail.com>
2021-04-02 20:23:11 -0400

    Statistics

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import "react-native";
+//import React from "react";
+//import App from "../App.js";
+//import renderer from "react-test-renderer";
+
+const data = [
+  { quarter: 1, earnings: 5 },
+  { quarter: 2, earnings: 10 },
+  { quarter: 3, earnings: 14 },
+  { quarter: 4, earnings: 1 },
+];
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <VictoryChart width={350} theme={VictoryTheme.material}>
+          <VictoryBar data={data} x="quarter" y="earnings" />
+        </VictoryChart>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
   },
 });
