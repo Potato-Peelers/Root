c9525a19a809ef0f1cf6cc6ebd76649391f1588f
VidulaMannem <73556634+VidulaMannem@users.noreply.github.com>
2021-05-31 23:17:27 -0400

    Update BottomTabNavigator.js

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {View,Text} from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import PantryListScreen from '../screens/PantryListScreen';
 import ShoppingListScreen from '../screens/ShoppingListScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import ScannerScreen from '../screens/ScannerScreen';
 import MealPlannerScreen from '../screens/MealPlannerScreen';
-import ExpirationDate from '../screens/ExpirationDate';
+// import ExpirationDate from '../screens/ExpirationDate';
 
 const BottomTabNavigator = createBottomTabNavigator({
-  One: PantryListScreen,
-  Two: ShoppingListScreen,
-  Three: SettingsScreen,
-  Four: ScannerScreen,
-  Five: MealPlannerScreen,
-  Six: ExpirationDate,
+  One: { screen: PantryListScreen, navigationOptions: { 
+    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-list-circle" size={35} color="black" />}},
+  Two: { screen: ShoppingListScreen, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-cart" size={35} color="black" />}},
+  Three: { screen: SettingsScreen, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-analytics" size={35} color="black" />}},
+  Four: { screen: ScannerScreen, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-camera" size={35} color="black" />}},
+  Five: { screen: MealPlannerScreen, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks-outline" size={35} color="black" />}},
+  
 }, {
   tabBarOptions: {
     showLabel: false
