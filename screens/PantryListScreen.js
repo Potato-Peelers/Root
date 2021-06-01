c4ca7df302cf8e4b5109e1af531d4ec83bd245bf
VidulaMannem <73556634+VidulaMannem@users.noreply.github.com>
2021-05-31 23:29:56 -0400

    Update PantryListScreen.js

--- screens/PantryListScreen.js
+++ screens/PantryListScreen.js
@@ -1,187 +1,220 @@
-import React, { Component } from 'react';
+//import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { View, StyleSheet, Text, FlatList, SafeAreaView, TouchableOpacity, TextInput, StatusBar,Button } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import firestore from '../src/firebase/config';
-
-import { StyleSheet, FlatList, Text, View, Alert, TouchableOpacity, TextInput,SafeAreaView, StatusBar } from 'react-native';
+import { LogBox } from 'react-native';
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 import { Ionicons } from '@expo/vector-icons';
 
-const TabIcon = (props) => (
-  <Ionicons
-    name={'md-list'}
-    size={35}
-    color={props.focused ? 'grey' : 'darkgrey'}
-  />
-)
-const docName ='qlw4YNAfCtUAJrSCFWly';
-export default class PantryListScreen extends React.Component {
+export default function PantryListScreen() {  // Expiration tracker
+  const navigationOptions = {
+    tabBarIcon: TabIcon
+  };
 
+const Color = {
+  primary: '#0CB0D3',
+  secondry: '#5FD9F3',
+  white: '#ffffff'
+}
+const user ='qlw4YNAfCtUAJrSCFWly';
+  const TabIcon = (props) => (
+    <Ionicons
+      name={'md-cart'}
+      size={35}
+      color={props.focused ? 'grey' : 'darkgrey'}
+    />
+  )
+  const [itemList, setItemList]=useState([])
+  const [itemName, setItemName] = useState("");
+  const [expDate,  setExpDate]  = useState(new Date(Date.now()));
+  const [datePickerVisibility, setDatePickerVisibility] = useState(true);
+
+  var db = firestore.collection('users').doc(user).collection('expirationdate').doc('list');
+
+  //console.log(" shopping list screen ")
+
+  useEffect(() => {
+    //fetchPantryItems();  // fetch it first time when app runs
+    getExpItems()
+    setDatePickerVisibility(true)
+  })
+
+  const addItem = () => {
+    //console.log("add exp item called")
+    db.collection("expiration").add({ // pantrylist is a collection containing item documents
+      name: itemName,
+      expiration: expDate.toUTCString()
+    });
+    setItemName("");
+    setExpDate(new Date(Date.now()))
+    getExpItems()
+  }
   
+
+  const updateItem = () => { // accept id as an argument
+    db.collection("expiration").doc(id).update({
+        type: "Unknown"
+    });
+  }
+
+  const getExpItems = () => {
+    //console.log("get exp items called ")
+    db.collection("expiration").get().then ((querySnapshot) => {
+      const list = querySnapshot.docs.map((doc) => ({
+        id : doc.id,
+        name: doc.data().name,
+        expiration: doc.data().expiration
+      }));
+      setItemList(list);
+      //console.log("itemlist", list)
+    });
+  }
+
+  const deleteItem = (id) => { // accept id as an argument
+    //console.log("delete Item called ")
+    db.collection("expiration").doc(id).delete();
+    getExpItems()
+  }
+  const handleConfirm = (date) => {
+    //console.warn("A date has been picked: ", date);
+    setExpDate(date)
+    hideDatePicker();
+  }
+  
+  const showDatePicker = () => {
+    console.log("show picker   true called ")
+    setDatePickerVisibility(true)
+  }
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false)
+  }
+  const renderItemComponent = (itemData) => {// 1
+  <TouchableOpacity>
+      <Text style={styles.buttonText}> {itemData.name} </Text>
+  </TouchableOpacity>
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.appButtonText}
+        placeholder="Enter Pantry Exp Item Here"
+        onChangeText={(itemName) => { setItemName( itemName )}}
+        value = { itemName }
+        underlineColorAndroid='transparent'
+      />
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePicker isVisible={datePickerVisibility}
+      mode="date" 
+      onConfirm={handleConfirm}
+      onCancel={hideDatePicker}
+      />
+      <Text style={styles.pickedDate}>{expDate.toUTCString()}</Text>
+      <TouchableOpacity onPress={() => addItem()} activeOpacity={0.7} style={styles.appButtonText} >
+        <Text style={styles.appButtonText}> Add Items </Text>
+
+      </TouchableOpacity>
+         <FlatList
+            data={itemList}
+            //renderItem={item => renderItemComponent(item)}
+           // renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+            renderItem={({ item, index }) => <TouchableOpacity key={index} onPress={()=>{deleteItem(item.id)}}><Text style={styles.item} >{item.name + " expires on  " + item.expiration}</Text></TouchableOpacity>}
+            keyExtractor={item => item.id.toString()}
+            //ItemSeparatorComponent={this.ItemSeparator}
+          /> 
+    </SafeAreaView>
+ );
+
+}
+
+/*export default class ShoppingListScreen extends React.Component {
+
+<FlatList
+        data={itemList}
+        numColumns={1}
+        ListEmptyComponent={
+          <View style={styles.flatListEmpty}>
+            <Text style={{fontWeight: 'bold'}}> Add Items Below </Text>
+          </View>
+        }
+        renderItem = {({ item }) => (
+          <View style={styles.flatListStyle}>
+            <Item title={item.name} />
+          </View>
+        )}
+      /> 
+
   static navigationOptions = {
     tabBarIcon: TabIcon
   };
- 
   constructor(props) {
-    
     super(props);
-
-    this.array = [{}
-    ],
-
-      this.state = {
-
-        arrayHolder: [],
-         textInput_Holder: ''
-        }
-        this.deleteData = this.deleteData.bind(this)
-        //var db = firestore.collection('users').doc(docName).collection('shoppinglist').doc('shoppinglist');
-        
-
   }
-componentDidMount() {
-    this.setState({ arrayHolder: [...this.array] })
-    //const [item, setItem] = useState('')
-    //const [newItem, setNewItem] = useState([null])
-    // useEffect hooks go here if class component is used
-    // use onSnapshot for getting updates automatically
- }
 
- joinData = () => {
-  this.array.push({title : this.state.textInput_Holder});
-  this.setState({ arrayHolder: [...this.array] })
-
-}
-deleteData = (t) =>{
-  //let itemsCopy = [...this.state.arrayHolder]
-  //itemsCopy.splice(t,1);
-  //this.setState({arrayHolder: itemsCopy});
-  //let itemsCopy = [...this.state.arrayHolder]
-  this.array.splice(t,1);
-  this.setState({ arrayHolder: [...this.array] })
-
-}
-
-addItem = () =>{
-  //setNewItem([text])
-  var text = this.state.textInput_Holder
-  var shoppingListDocRef = firestore.collection('users').doc(docName).collection('shoppinglist').doc('week2');
-    //var shoppingListDocRef = firestore.collection('users').doc(docName).collection('shoppinglist').doc('shoppinglist');
-    //shoppingListDocRef.collection("week1").add({text})
-    //var newItemRef = shoppingListDocRef.collection("week1").add({text})
-    //newItemRef.set(text)
-
-   shoppingListDocRef.set({text});
-
-   // also show on the UI
-   this.array.push({title : this.state.textInput_Holder});
-  this.setState({ arrayHolder: [...this.array] })
-}
-
-FlatListItemSeparator = () => {
-  return (
-    <View
-      style={{
-        height: 1,
-        width: "100%",
-        backgroundColor: "#607D8B",
-      }}
-    />
+  render() {
+    const list = [
+      {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'Bagels',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Cream Cheese',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Orange Juice',
+      },
+    ];
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
   );
-}
-/*
-GetItem(item) {
+  
+  const renderItem = ({ item }) => (
+      <Item title={item.title} />
+  );
 
-  Alert.alert(item);
-
-}
-*/
-
-render() {
   return (
-
-    <SafeAreaView style={styles.container}>
-
-      <TextInput
-        placeholder="Enter Item Here"
-        onChangeText={data => this.setState({ textInput_Holder: data })}
-        style={styles.textInputStyle}
-        underlineColorAndroid='transparent'
-      />
-
-      <TouchableOpacity onPress={this.addItem} activeOpacity={0.7} style={styles.button} >
-
-        <Text style={styles.buttonText}> Add Items to Pantry </Text>
-
-      </TouchableOpacity>
       <SafeAreaView style={styles.container}>
-          <FlatList
-
-          data={this.state.arrayHolder}
-
-          width='100%'
-
-          extraData={this.state.arrayHolder}
-
-          keyExtractor={(index) => index.toString()}
-
-          ItemSeparatorComponent={this.FlatListItemSeparator}
-
-          renderItem={({ item, index }) => <TouchableOpacity key={index} onPress={()=>{this.deleteData(index)}}><Text>{item.title}</Text></TouchableOpacity>}
-          />
-        </SafeAreaView>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
       </SafeAreaView>
+   );
+  }
+}*/
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 50,
+      height: 50,
+      padding: 10,
+    },
+    item: {
+      backgroundColor: '#0CB0D3',
+      padding: 10,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 22,
+    },
+    pickedDate: {
+      fontSize: 18,
+      color: 'black',
+      padding: 10,
+      alignSelf: 'center',
+    },
+    appButtonText: {
+      fontSize: 18,
+      color: 'green',
+      alignSelf: "center",
+      padding: 10,
+    }
+  });
 
-  );
-}
-}
-
-const styles = StyleSheet.create({
-
-  MainContainer: {
-
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    margin: 2
-
-  },
-
-  pitem: {
-    padding: 15,
-    fontSize: 20,
-    height: 48,
-  },
-
-  textInputStyle: {
-
-    textAlign: 'center',
-    height: 40,
-    width: '90%',
-    borderWidth: 1,
-    borderColor: '#4CAF50',
-    borderRadius: 7,
-    fontSize: 18,
-    marginTop: 12
-  },
-
-  button: {
-
-    width: '90%',
-    height: 40,
-    padding: 10,
-    backgroundColor: '#4CAF50',
-    borderRadius: 8,
-    fontSize: 18,
-    marginTop: 10
-  },
-
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-
-});
